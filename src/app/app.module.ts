a091ce973db1fc2c2e2dca30abdf0d7a8bb186cd
Grzegorz <kontakt@gpietrzak.pl>
2021-01-08 21:57:13 +0100

    added: material2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WeatherComponent } from './weather/weather.component';
+
+import { MaterialModule } from './shared/material.module'
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
